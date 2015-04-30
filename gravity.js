1f737b1c88369d17d2ae6244e0f512e0cebf793f
EreminDm <eremin.dm@gmail.com>
2015-04-30 21:12:05 +0600

    Гравити
    БЕЗ конвертора

---
+++ gravity.js
@@ -0,0 +1,46 @@
+var massGi=[9.8,3.711];
+for (i=0;i<massGi.length;i++){
+
+	
+	g=massGi[i];
+	console.log ("Ускорение = "+g);
+		
+		
+		
+	function Dist (t) {
+		var t2 =Math.pow (t,2);
+		var Rast = g*t2/2;
+		return Rast;
+		}
+	function Speed (t){
+		var speed=g*t;
+		return speed;
+		}
+	function Time (h) {
+		var time = 2*h/g;
+		var time1 = Math.sqrt(time);
+		return time1;
+		}
+	function EndSpeed (h) {
+		var speed=2*g*h;
+		var speedsqr= Math.sqrt(speed);
+		return speedsqr;	
+		}
+
+
+	var l=20;
+	var p=200;
+	j1=Dist(l);
+	j2=Speed(l);
+	j3=Time(p);
+	j4=EndSpeed(p);
+
+
+	console.log ("Растояние за "+l+"сек= "+ j1);
+	console.log ("Скорость по прошествии "+l+"сек="+j2);
+	console.log ("Время свободного падения с высоты "+l+"м="+j3);
+	console.log ("Скорость падения с высоты "+p+"м="+j4);
+	
+}
+	
+	
